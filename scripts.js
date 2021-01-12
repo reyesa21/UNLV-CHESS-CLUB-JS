9ca59fa7f70e394ad792b2fd78404b6137b654c9
reyesa21 <a99reyes@gmail.com>
2021-01-11 22:38:59 -0800

    updated contact section, made join section more intuitive, added read more to members

--- scripts.js
+++ scripts.js
@@ -8,28 +8,51 @@ function nav() {
   } 
 
 function popUpForm(){
+
     var x = document.getElementById("signUp");
     var dim = document.getElementById("dimmer");
-    dim.style.display = 'none';
-    if (x.style.display === "none") {
-      x.style.display = "block";
-      dim.style.display = "block";
-    } else {
+
+    if (x.style.display === "block") {
       x.style.display = "none";
       dim.style.display = "none";
+      body.style.overflow = "unset";
+    } else {
+      x.style.display = "block";
+      dim.style.display = "block";
+      body.style.overflow = "hidden";
     }  
-
+    console.log(x);
   }
 
  window.onload = () =>{
       var div = document.getElementById("signUp");
       var dim = document.getElementById("dimmer");
-
       document.onclick = (e) =>{
          if(div.style.display ==="block" && e.target.id === 'dimmer'){
             div.style.display = 'none';
             dim.style.display = 'none';
-         }
+            body.style.overflow = "unset";
+        }
 
       };
    };
+
+
+   function readMore() {
+    var dots = document.getElementById("dots");
+    var moreText = document.getElementById("more");
+    var btnText = document.getElementById("readMoreBtn");
+  
+
+    if (dots.style.display === "none") {
+      dots.style.display = "inline";
+      btnText.innerHTML = "Read More";
+      moreText.style.display = "none";
+    } else {
+      dots.style.display = "none";
+      btnText.innerHTML = "Read Less";
+      moreText.style.display = "inline";
+    }
+  }
+
+  
